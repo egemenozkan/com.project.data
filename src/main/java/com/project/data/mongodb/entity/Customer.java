a8f15b1690f2e282025296f3018bb3828b0c7366
Egemen <egemenozkan@gmail.com>
2020-05-06 22:04:11 +0300

    mongodb

---
+++ src/main/java/com/project/data/mongodb/entity/Customer.java
@@ -0,0 +1,66 @@
+package com.project.data.mongodb.entity;
+
+import org.springframework.data.annotation.Id;
+
+import com.project.api.data.enums.Gender;
+
+public class Customer {
+
+	@Id
+	private String id;
+
+	private String firstName;
+	private String middleName;
+	private String lastName;
+	private Gender gender;
+	private String birthdate;
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getMiddleName() {
+		return middleName;
+	}
+
+	public void setMiddleName(String middleName) {
+		this.middleName = middleName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public Gender getGender() {
+		return gender;
+	}
+
+	public void setGender(Gender gender) {
+		this.gender = gender;
+	}
+
+	public String getBirthdate() {
+		return birthdate;
+	}
+
+	public void setBirthdate(String birthdate) {
+		this.birthdate = birthdate;
+	}
+
+}
---
+++ src/main/java/com/project/data/mongodb/repository/CustomerRepository.java
@@ -0,0 +1,15 @@
+package com.project.data.mongodb.repository;
+
+import java.util.List;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+import com.project.data.mongodb.entity.Customer;
+
+public interface CustomerRepository extends MongoRepository<Customer, String> {
+
+	public Customer findByFirstName(String firstName);
+
+	public List<Customer> findByLastName(String lastName);
+
+}
