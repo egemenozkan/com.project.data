426de5d91c2201e4ba6894405b099a7165add6f1
egemenozkan@gmail.com <egemenozkan@gmail.com>
2020-08-09 22:00:31 +0300

    inital comment

--- deploy.sh
+++ deploy.sh
@@ -2,4 +2,4 @@ mvn -f ~/Devel/workspace/com.project.contract/ clean install
 
 mvn clean install package -DskipTests -Pprod
 
-scp target/data-alfa.jar devops@94.237.94.131:/var/www/jee_projects/datapool
+scp target/data-alfa.jar devops@94.237.97.137:/var/www/jee_projects/datapool
